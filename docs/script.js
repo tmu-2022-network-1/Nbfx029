357fe9c378e15af80d19ffc86f5dce7ed7ab70e5
Nbfx029 <nebelfoxx48@gmail.com>
2022-07-17 20:35:49 +0900

    a

---
+++ docs/script.js
@@ -0,0 +1,10 @@
+var scrollAnimationElm = document.querySelectorAll('.sa');
+var scrollAnimationFunc = function() {
+  for(var i = 0; i < scrollAnimationElm.length; i++) {
+    var triggerMargin = 300;
+    if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+      scrollAnimationElm[i].classList.add('show');
+    }
+  }
+}
+window.addEventListener('scroll', scrollAnimationFunc);
