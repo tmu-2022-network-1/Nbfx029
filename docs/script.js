5a04d4129b65ac11d05914a0ca79d01f4e323e1c
Nbfx029 <nebelfoxx48@gmail.com>
2022-07-19 09:59:15 +0900

    わあ

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,63 @@
-var scrollAnimationElm = document.querySelectorAll('.sa');
+/*var scrollAnimationElm = document.querySelectorAll('.sa');
 var scrollAnimationFunc = function() {
   for(var i = 0; i < scrollAnimationElm.length; i++) {
-    var triggerMargin = 300;
-    if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+    if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + 100) {
       scrollAnimationElm[i].classList.add('show');
+    }
+    if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + 700) {
+      scrollAnimationElm[i].classList.add('show');
+    }
+    if(window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + 750){
+      scrollAnimationElm[i].classList.remove('show');
+    }
+    if(window.innerHeight < scrollAnimationElm[i].getBoundingClientRect().top){
+      scrollAnimationElm[i].classList.remove('show');
     }
   }
 }
 window.addEventListener('scroll', scrollAnimationFunc);
+*/
+ScrollTrigger.create({
+  trigger: '#sora',
+  start: 'top 800px',
+  end: 'bottom 200px',
+  toggleClass: {targets: "#sora", className: "show"},
+});
+
+ScrollTrigger.create({
+  trigger: '#mado',
+  start: 'top 800px',
+  end: 'bottom 200px',
+  toggleClass: {targets: "#mado", className: "show"},
+});
+
+gsap.to("#hu", {
+  autoAlpha: 1,
+  x: 20,
+  rotation: 30, 
+  duration: 2,
+  scrollTrigger: {
+    trigger: "#hu",
+    start: 'top center',
+    end: '+=500',
+    scrub: true
+  }
+});
+
+gsap.to("#mado", {
+  rotation: 30,
+  duration: 2.5,
+  scrollTrigger: {
+    trigger: "#mado",
+    start: 'top center',
+    end: '+=500',
+    markers: true,
+  }
+});
+
+/*window.addEventListener('scroll', () => {
+  let elem00 = document.getElementById('sign');
+  let scrollY = window.scrollY/15;
+  
+  elem00.style.filter.blur =  scrollY + 'px';
+});*/
